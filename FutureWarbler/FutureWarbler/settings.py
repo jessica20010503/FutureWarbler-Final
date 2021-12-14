9d09abbaec25aaeb9538b3d03200d86522f3f6a2
vivian153 <mina19991014@gmail.com>
2021-12-14 16:24:31 +0800

    login successfully

--- FutureWarbler/FutureWarbler/settings.py
+++ FutureWarbler/FutureWarbler/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'FutureWarbler.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
+        'NAME': 'futurewarbler',
         'USER': 'root',
-        'PASSWORD': 'FutureWarbler301',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
-        'PORT': '3307',
+        'PORT': '3306',
     }
 }
 
--- FutureWarbler/myapp/views.py
+++ FutureWarbler/myapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -9,3 +13,19 @@ def register(request):
     return render(request,"register.html",locals())
 def personal(request):
     return render(request,"personal-page.html",locals())
+
+#登入
+def login(request):
+    if request.method == 'POST':
+        account = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=account, password=password)
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+                return redirect('/index')
+            else:
+                message = '帳號未啟用!'
+        else:
+            message = '登入失敗!'
+    return render(request, "login.html", locals())
