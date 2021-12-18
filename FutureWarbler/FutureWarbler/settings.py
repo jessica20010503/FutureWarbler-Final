3fed44b520a3b311a59552b6721c07605a75973e
YunTingJian <tifany90123281@gmail.com>
2021-12-18 08:53:09 +0800

    tiffany_update_修正註冊及登入細節

--- FutureWarbler/FutureWarbler/settings.py
+++ FutureWarbler/FutureWarbler/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
         'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': '3306',
+       'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", #防止migrate時出現報錯
+    },
     }
 }
 
