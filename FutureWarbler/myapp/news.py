d2ab2feaa69bfa69b7a5bf015f01b1003ed4e1f0
szuyulien <87714836+szuyulien@users.noreply.github.com>
2021-12-31 18:07:26 +0800

    新聞更新2

--- FutureWarbler/myapp/news.py
+++ FutureWarbler/myapp/news.py
@@ -3,18 +3,21 @@ import requests as rq
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
-import requests
 import random
 from datetime import datetime
 import sys
 import re
- 
+
 news_title = []
 news_content = []
 news_time = []
 news_author= []
 news_photo=[]
 news_area = []
+news_type = []
+# type 0是最新 1熱門
+news_category = []
+# category 0 財經 1 期貨 2 兩岸 3  國際 4產業 5理財
 df = pd.DataFrame()
 #財經總覽最新
 home_url = "https://money.udn.com/rank/newest/1001/0"
@@ -31,6 +34,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(0)
+    news_type.append(0)
+    news_category.append(0)
     time.sleep(1)
 #財經總覽最熱
 home_url = "https://money.udn.com/rank/pv/1001/0"
@@ -48,6 +53,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(1)
+    news_type.append(1)
+    news_category.append(0)
     time.sleep(1)
 #最新期貨
 home_url = "https://money.udn.com/rank/newest/1001/11111"
@@ -64,6 +71,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(2)
+    news_type.append(0)    
+    news_category.append(1)
     time.sleep(1)
 
 #最熱門期貨
@@ -81,6 +90,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(3)
+    news_type.append(1)
+    news_category.append(1)
     time.sleep(1)
 
 
@@ -99,6 +110,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(4)
+    news_type.append(0)
+    news_category.append(2)
     time.sleep(1)
 #兩岸最熱
 home_url = "https://money.udn.com/rank/pv/1001/5589"
@@ -115,6 +128,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(5)
+    news_type.append(1)
+    news_category.append(2)
     time.sleep(1)
 #國際最新
 home_url = "https://money.udn.com/rank/newest/1001/5588"
@@ -131,6 +146,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(6)
+    news_type.append(0)
+    news_category.append(3)
     time.sleep(1)
 #國際最熱
 home_url = "https://money.udn.com/rank/pv/1001/5588"
@@ -147,6 +164,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(7)
+    news_type.append(1) 
+    news_category.append(3)
     time.sleep(1)
 
 #產業最新
@@ -164,6 +183,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(8)
+    news_type.append(0)
+    news_category.append(4)
     time.sleep(1)
 #產業最熱
 home_url = "https://money.udn.com/rank/pv/1001/5591"
@@ -180,6 +201,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(9)
+    news_type.append(1)
+    news_category.append(4)
     time.sleep(1)
 
 #理財最新
@@ -197,6 +220,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(10)
+    news_type.append(0)
+    news_category.append(5) 
     time.sleep(1)
 #理財最熱
 home_url = "https://money.udn.com/rank/pv/1001/5592"
@@ -213,6 +238,8 @@ for i in range(0, len(soup.select("div.story__content"))):
     news_time.append(soup_content.find("time","article-body__time").text)
     news_photo.append(soup.select("div.story__image")[i].select("img")[0]["src"])
     news_area.append(11)
+    news_type.append(1)
+    news_category.append(5)
     time.sleep(1)
 df["news_title"]=news_title  
 df["news_content"]=news_content
@@ -220,6 +247,9 @@ df["news_time"]=news_time
 df["news_author"]=news_author
 df["news_photo"]=news_photo
 df["news_area"]=news_area
+df["news_type"]=news_type
+df["news_category"]=news_category
 from sqlalchemy import create_engine
-conn = create_engine('mysql+pymysql://root:12345678@127.0.0.1:3306/futurewarbler?charset=utf8')  
+conn = create_engine('mysql+pymysql://root:12345678@127.0.0.1:3306/futurewarbler?charset=utf8mb4')  
 pd.io.sql.to_sql(df,'news',con=conn,if_exists='append')
+
