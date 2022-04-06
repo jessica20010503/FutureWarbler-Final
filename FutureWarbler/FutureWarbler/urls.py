9a61b65412a3d8d6f59671f2054ab76d08ed4662
DADA2816 <anniecheng828@gmail.com>
2022-04-06 23:36:04 +0800

    期貨小教室側欄連結、分頁active

--- FutureWarbler/FutureWarbler/urls.py
+++ FutureWarbler/FutureWarbler/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp.views import index, login, register, personal, classes, classcontent, indexclass, indexclasscontent, robotnormal, robotintelligent, news, news1, newscontent, newssearch, trade, transactionRecord, strategy, logout, personal_unlogin, update, contract, order, strategy_normal, send_strategy_sql, test
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -45,5 +46,6 @@ urlpatterns = [
     path('strategy_normal/', strategy_normal),
     path('send_strategy_sql/', send_strategy_sql),
     path('test/', test),
-
+    # path('api/GetTechnicalImgHeml', views.GetTechnicalImgHeml.as_view()),
+    path('api/GetTechnicalType', views.GetTechnicalType.as_view()),
 ]
